cd126408ac37713bf04c909917f65dbee74feafd
JuriKovtun <kovtun.yurko@gmail.com>
2019-11-07 17:55:43 +0200

    problem solved

--- vjudge_l.py
+++ vjudge_l.py
@@ -1,1 +1,25 @@
-n = int(input())
+multiplier = int(input())
+
+
+def sum_digits(n):
+    a = n % 10
+    bb = n % 100
+    b = bb // 10
+    c = n // 100
+    return a + b + c
+
+
+counter = 0
+
+for i in range(10, 100):
+
+    s = sum_digits(i)
+    new_i = i * multiplier
+    new_s = sum_digits(new_i)
+
+    if s == new_s:
+        counter += 1
+
+print(counter)
+
+
